0b60328e5b879cc86b340080d6228ce044478a99
daffashafwan <daffashafwan321@gmail.com>
2022-03-07 13:05:55 +0700

    add init config db main

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+import (
+	_mysqlDriver "github.com/daffashafwan/taskmajoo/config"
+	
+	"log"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+)
+
+func init() {
+	viper.SetConfigFile(`config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	if viper.GetBool(`debug`) {
+		log.Println("Service RUN on DEBUG mode")
+	}
+}
+
+func main() {
+	// init koneksi databse
+	configDB := _mysqlDriver.ConfigDB{
+		DB_Username: viper.GetString(`database.user`),
+		DB_Password: viper.GetString(`database.pass`),
+		DB_Host:     viper.GetString(`database.host`),
+		DB_Port:     viper.GetString(`database.port`),
+		DB_Database: viper.GetString(`database.name`),
+	}
+}
